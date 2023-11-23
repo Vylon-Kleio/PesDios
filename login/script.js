e2af86fc3c4da588d41729fbf7d7f472880bf86f
Vylon-Kleio <rafaelmarchel.dw@gmail.com>
2023-11-23 21:10:35 +0700

    Optimize the login page
    login page now work with admin as username and password. In addition,
    pressing the shortcut SHIFT + CTRL + ALT + P, will also redirect to the
    dashboard.
    
    Create the initial design of the dashboard (sidebar panel)

--- login/script.js
+++ login/script.js
@@ -10,12 +10,21 @@ login.addEventListener("click", () => {
   wrapper.classList.remove("active");
 });
 
+document.addEventListener("keydown", (e) => {
+  if (e.key.toLowerCase() === "p" && e.ctrlKey && e.altKey && e.shiftKey) {
+    location.href = "../dashboard/dashboard.html";
+  }
+});
+
 function validate() {
   const username = document.querySelector("#user-login").value;
   const password = document.querySelector("#pass-login").value;
-  if (username == "admin" && password == "admin") {
+  if (username === "admin" && password === "admin") {
     location.href = "../dashboard/dashboard.html";
+    console.log("Login successful");
+    return false;
   } else {
     alert("Login failed");
+    return false;
   }
 }
